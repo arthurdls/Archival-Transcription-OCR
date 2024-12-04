bca47738a6c9fa3a7a7d37db91da840f99017786
Arthur De Los Santos <arthurdls04@gmail.com>
2024-12-03 22:48:04 -0500

    add testing of our synthetic data

---
+++ DTrOCR_our_test.py
@@ -0,0 +1,175 @@
+import os
+import torch
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
+import multiprocessing as mp
+from typing import Tuple
+import tqdm
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+
+from DTrOCR.dtrocr.processor import DTrOCRProcessor, modified_build_inputs_with_special_tokens
+from DTrOCR.dtrocr.config import DTrOCRConfig
+from DTrOCR.dtrocr.model import DTrOCRLMHeadModel
+
+seed = 42
+random.seed(seed)
+np.random.seed(seed)
+torch.manual_seed(seed)
+torch.cuda.manual_seed_all(seed)
+
+# os.environ['HDF5_USE_FILE_LOCKING']='FALSE'
+# os.environ['TF_ENABLE_ONEDNN_OPTS']='0'
+# os.environ['XLA_FLAGS']='--xla_gpu_cuda_data_dir=/usr/local/pkg/cuda/cuda-11.8'
+# os.environ['TF_GPU_ALLOCATOR']='cuda_malloc_async'
+# os.environ['HF_HUB_OFFLINE']='1'
+# torch.multiprocessing.set_sharing_strategy('file_system')
+
+files = {
+    'noise_word':[],
+    'noise_word_blur':[],
+    'noise_word_distort':[],
+    'sepia_word':[],
+    'sepia_word_blur':[],
+    'sepia_word_distort':[],
+    'white_word':[],
+    'white_word_blur':[],
+    'white_word_distort':[]
+}
+
+for file_name, test_data_list in files.items():
+    with open(f'../single_word/{file_name}/labels.txt', 'r') as file:
+        for line in file:
+            img_path, target = line.replace(",", "").split(" ")
+            datapoint = {
+                'image_path': os.getcwd() + f'/../single_word/{file_name}/' + img_path,
+                'text': target
+            }
+            test_data_list.append(datapoint)
+
+# # Data Loader
+
+class OurDataset(Dataset):
+    def __init__(self, words, config: DTrOCRConfig):
+        super(OurDataset, self).__init__()
+        self.words = words
+        self.processor = DTrOCRProcessor(config, add_eos_token=True, add_bos_token=True)
+
+    def __len__(self):
+        return len(self.words)
+
+    def __getitem__(self, item):
+        with Image.open(self.words[item]['image_path']).convert('RGB') as img:
+            inputs = self.processor(
+                images= img,
+                texts=self.words[item]['text'],
+                padding='max_length',
+                return_tensors="pt",
+                return_labels=True,
+            )
+        return {
+            'pixel_values': inputs.pixel_values[0],
+            'input_ids': inputs.input_ids[0],
+            'attention_mask': inputs.attention_mask[0],
+            'labels': inputs.labels[0]
+        }
+
+config = DTrOCRConfig(
+    max_position_embeddings = 512
+)
+
+files_with_dataloaders = {}
+
+for file_name, test_data_list in files.items():
+    test_data = OurDataset(words=test_data_list, config=config)
+    test_dataloader = DataLoader(test_data, batch_size=32, shuffle=False, num_workers=mp.cpu_count())
+    files_with_dataloaders[file_name] = test_dataloader
+
+# Model
+
+print("Loading Model...")
+
+if torch.cuda.is_available():
+    device = torch.device("cuda")  
+    print("Using GPU:", torch.cuda.get_device_name(0))
+else:
+    device = torch.device("cpu")
+    print("Using CPU")
+
+torch.set_float32_matmul_precision('high')
+
+model = DTrOCRLMHeadModel(config)
+model = torch.compile(model)
+model.to(device=device)
+print(model)
+
+
+def evaluate_model(model: torch.nn.Module, dataloader: DataLoader) -> Tuple[float, float]:
+    # set model to evaluation mode
+    model.eval()
+
+    losses, accuracies = [], []
+    with torch.no_grad():
+        for inputs in tqdm.tqdm(dataloader, total=len(dataloader), desc=f'Evaluating test set'):
+            inputs = send_inputs_to_device(inputs, device=0)
+            outputs = model(**inputs)
+
+            losses.append(outputs.loss.item())
+            accuracies.append(outputs.accuracy.item())
+
+    loss = sum(losses) / len(losses)
+    accuracy = sum(accuracies) / len(accuracies)
+
+    # set model back to training mode
+    model.train()
+
+    return loss, accuracy
+
+def send_inputs_to_device(dictionary, device):
+    return {key: value.to(device=device) if isinstance(value, torch.Tensor) else value for key, value in dictionary.items()}
+
+for file_name, test_dataloader in files_with_dataloaders.items():
+    test_loss, test_accuracy = evaluate_model(model, test_dataloader)
+
+    print(f"{file_name}:: Loss: {test_loss} - Accuracy: {test_accuracy}")
+
+
+# Testing
+
+# from datasets import load_dataset
+# IAM = load_dataset("Teklia/IAM-line")
+# SROIE = load_dataset("rth/sroie-2019-v2")
+
+# model.load_state_dict(torch.load('./epoch_7_checkpoint_model_state_dict.pt', weights_only=True))
+# model.eval()
+# model.to('cpu')
+# test_processor = DTrOCRProcessor(config)
+
+
+# for datapoint in test_data_list:
+#     with Image.open(datapoint['image_path']).convert('RGB') as img:
+#         image = img
+#     actual_text = datapoint['text']
+
+#     inputs = test_processor(
+#         images=image.convert('RGB'),
+#         texts=test_processor.tokeniser.bos_token,
+#         return_tensors='pt'
+#     )
+
+#     model_output = model.generate(
+#         inputs,
+#         test_processor,
+#         num_beams=3
+#     )
+
+#     predicted_text = test_processor.tokeniser.decode(model_output[0], skip_special_tokens=True)
+#     print(f"Actual: {actual_text}, Predicted: {predicted_text}")
+
+#     plt.figure(figsize=(10, 5))
+#     plt.title(predicted_text, fontsize=24)
+#     plt.imshow(np.array(image, dtype=np.uint8))
+#     plt.xticks([]), plt.yticks([])
+#     plt.savefig("plots/plot.png")
